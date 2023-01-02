126db1c282d5414782a1f95f72b52b902335ee66
JavApext <JavApext@gmail.com>
2023-01-02 14:07:00 +0900

    2022/01/02 파이썬

---
+++ Backjun/1번.py
@@ -0,0 +1,3 @@
+a, b = map(int, input().split()) # map 함수로 일괄적용
+
+print(a+b)
---
+++ Ch01/hello.py
@@ -0,0 +1,13 @@
+"""
+날짜 : 2023/01/02
+이름 : 길은섭
+내용 : Python Hello World
+
+ - 번수 타입 선언 안해도 됨
+ - 큰, 작은 따옴표 구분안함
+ - 코딩테스트는 파이썬이 편하고 빠름
+"""
+
+print('Hello World')
+print('Hello Python')
+print("안녕하세요. 파이썬")
